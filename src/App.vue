9f32efa95c99625674db23fbf673d72c90c35f86
Joseph Kabemba <joseph.kabemba@academy.kinshasadigital.com>
2021-07-16 03:05:06 +0200

    feat: create and add to app ProductOne and ProductTwo components

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <!-- <h1>Vuex: Beginners</h1>
+    <ul v-for="product in products" :key="products.indexOf(product.name)">
+      <li>{{ product.name }}</li>
+    </ul> -->
+    <ProductOne v-bind:products="products" />
+    <ProductTwo v-bind:products="products" />
+  </div>
+</template>
+
+<script>
+import ProductOne from "./components/ProductOne.vue";
+import ProductTwo from "./components/ProductTwo.vue";
+
+export default {
+  name: "App",
+  components: {
+    ProductOne,
+    ProductTwo,
+  },
+  data() {
+    return {
+      products: [
+        { name: "Banana Skin", price: 20 },
+        { name: "Skinny Star", price: 40 },
+        { name: "Green Shells", price: 60 },
+        { name: "Red Shells", price: 80 },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Ubuntu;
+  color: #555;
+}
+</style>
---
+++ src/components/ProductTwo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="product-list-two">
+    <h1>Product List Two</h1>
+    <ul>
+      <li v-for="product in products" :key="products.indexOf(product.name)">
+        <span class="name">{{ product.name }}: </span>
+        <span class="price">£{{ product.price }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductTwo",
+  props: ["products"],
+};
+</script>
+
+<style scoped>
+#product-list-two {
+  background: #d1e4ff;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
+}
+#product-list-two ul {
+  padding: 0;
+  list-style-type: none;
+}
+#product-list-two li {
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.7);
+}
+.price {
+  font-weight: bold;
+  color: #860ce8;
+  display: block;
+}
+</style>
